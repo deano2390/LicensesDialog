f1e8d352dd818639623f2c3725defb76a73901dc
Dean Wild <dean.wild@wizix.com>
2016-09-13 12:35:40 +0100

    gradle-a-fy the project for use in Android Studio

---
+++ sample/src/main/java/de/psdev/licensesdialogsample/sample/MainActivity.java
@@ -0,0 +1,33 @@
+package de.psdev.licensesdialogsample.sample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
+import de.psdev.licensesdialogsample.R;
+
+public class MainActivity extends FragmentActivity {
+
+    // ==========================================================================================================================
+    // Android Lifecycle
+    // ==========================================================================================================================
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // ==========================================================================================================================
+    // Public API
+    // ==========================================================================================================================
+
+    public void onSampleClick(final View view) {
+        startActivity(new Intent(this, SampleActivity.class));
+    }
+
+    public void onAppCompatClick(final View view) {
+        startActivity(new Intent(this, AppCompatSampleActivity.class));
+    }
+}
